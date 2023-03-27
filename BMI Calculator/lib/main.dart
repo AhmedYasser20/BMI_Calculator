eeca8708fa777f6a5865273fd7a1ee6e70d602e3
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2023-03-27 18:57:37 +0200

    Update main.dart

--- BMI Calculator/lib/main.dart
+++ BMI Calculator/lib/main.dart
@@ -1,10 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 // ignore_for_file: prefer_const_literals_to_create_immutables
-import 'package:app1/HomeScreen.dart';
-import 'package:app1/Login_Screen.dart';
-import 'package:app1/MessengerScreen.dart';
-import 'package:app1/userScreen.dart';
-import 'package:app1/CounterScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:app1/bmi_Screen.dart';
 import 'package:app1/bmi_result_screen.dart';
